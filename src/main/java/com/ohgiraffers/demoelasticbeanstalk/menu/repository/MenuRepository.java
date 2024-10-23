9b56b7ee7c805ee2b2b934ee88647e6bc8bdc0a0
INUK-ai <arheon2469@gmail.com>
2024-10-23 14:47:42 +0900

    Setting: Project

---
+++ src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/repository/MenuRepository.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.demoelasticbeanstalk.menu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.awt.*;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
